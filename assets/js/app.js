d0ceb22605f1ad81ba77e7af5b6992205ddcc2b6
Nuruddin-Adnan <contact.codepixel@gmail.com>
2022-09-03 16:50:51 +0600

    news count under a category

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,14 @@
+// loader 
+const loader = isloading => {
+    const loaderElement = document.getElementById('loader')
+    if (isloading) {
+        loaderElement.classList.remove('d-none')
+    } else {
+        loaderElement.classList.add('d-none')
+    }
+}
+
+
 // load all categories from API
 const loadCategory = async () => {
     try {
@@ -43,11 +54,13 @@ const showCategory = categories => {
 
 // load news by category
 const loadNewsByCategory = async (categoryId = '08') => {
+    loader(true)
     const url = `https://openapi.programming-hero.com/api/news/category/${categoryId}`;
     try {
         const res = await fetch(url);
         const data = await res.json();
-        showNewsByCategory(data.data)
+        showNewsByCategory(data.data);
+        newsUnderCategory(data.data.length, data.data);
     } catch (error) {
         console.log('no data found');
     }
@@ -56,14 +69,16 @@ loadNewsByCategory();
 
 
 
-
-
-
-
-
+// show news by category
 const showNewsByCategory = newsArray => {
     const newsElement = document.getElementById('news');
     newsElement.innerHTML = '';
+
+    if (newsArray.length === 0) {
+        newsElement.innerHTML = `
+        <p class="text-center text-danger">No Data Found</p>
+        `;
+    }
     newsArray.forEach(news => {
         const article = document.createElement('article');
         article.classList.add('news-item', 'mb-4');
@@ -76,19 +91,19 @@ const showNewsByCategory = newsArray => {
                 </div>
                 <div>
                     <h3 class="title">${news.title}</h3>
-                    <p class="news-description">${news.details}</p>
+                    <p class="news-description my-3"> ${(news.details).slice(0, 350)}${(news.details).length > 350 ? '...' : ''}</p>
                     <div class="d-lg-flex align-items-center justify-content-between">
                         <div class="author-info d-flex align-items-center">
                             <div class="flex-shrink-0 me-2">
-                                <img src="assets/image/author.png" alt="author" class="rounded-circle" width="40" height="40">
+                                <img src="${news.author.img}" alt="author" class="rounded-circle" width="40" height="40">
                             </div>
                             <div>
-                                <h6 class="author-name mb-0">Jane Cooper</h6>
-                                <p class="news-date mb-0">Jan 10, 2022 </p>
+                                <h6 class="author-name mb-0">${news.author.name ? news.author.name : 'No data found'}</h6>
+                                <p class="news-date mb-0">${news.author.published_date ? news.author.published_date : 'No data found'}</p>
                             </div>
                         </div>
                         <div class="d-flex align-items-center justify-content-between news-action mt-lg-0 mt-3">
-                            <p class="mb-0 news-view text-center"><i class="far fa-eye pe-2"></i>1.5M</p>
+                            <p class="mb-0 news-view text-center"><i class="far fa-eye pe-2"></i>${news.total_view ? news.total_view : 'No data found'}M</p>
                             <div class="rating text-center">
                                 <i class="fas fa-star-half-alt"></i>
                                 <i class="far fa-star"></i>
@@ -106,9 +121,17 @@ const showNewsByCategory = newsArray => {
         </div>
         `
 
-        newsElement.appendChild(article)
-        console.log(news);
+        newsElement.appendChild(article);
     });
+
+    loader(false)
+}
+
+// news under a single category
+const newsUnderCategory = (count, categoryName) => {
+    const newsCountElement = document.getElementById('category-item-count');
+    newsCountElement.innerText = count
+    console.log(categoryName);
 }
 
 
